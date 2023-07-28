3391a64b5967d0fa337582ac15dba80c71701588
MARY NJOKI <marynjoki738@gmail.com>
2023-07-28 08:33:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const alphanumericStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  
+  // Compare characters from the beginning and end of the string
+  for (let i = 0; i < alphanumericStr.length / 2; i++) {
+    if (alphanumericStr[i] !== alphanumericStr[alphanumericStr.length - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
+}
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
